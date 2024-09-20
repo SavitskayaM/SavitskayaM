4852a6d172f85973d408b51b17dc3fb937ad22ae
SavitskayaM <savitskayari@yandex.ru>
2024-09-20 13:53:14 +0300

    Update lab1_1.java

--- src/lab1/lab1_1.java
+++ src/lab1/lab1_1.java
@@ -7,7 +7,7 @@ class DivisibleByTenThread extends Thread {
             for (int i = 0; i <= 100; i++) {
                 if (i % 10 == 0) {
                     System.out.println(i);
-                    Thread.sleep(500); // Pause for 500 milliseconds (0.5 seconds)
+                    Thread.sleep(500); 
                 }
             }
         } catch (InterruptedException e) {
