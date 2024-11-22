479924e3f5b8446e3de410b0a1481c41c979a670
SavitskayaM <savitskayari@yandex.ru>
2024-11-22 22:50:27 +0300

    Update lab1_3.java

--- src/lab1/lab1_3.java
+++ src/lab1/lab1_3.java
@@ -1,6 +1,12 @@
-package lab1;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
-class lab1_3 implements Runnable {
+class TimerTask implements Runnable {
+    private final CyclicBarrier barrier;
+
+    public TimerTask(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
 
     @Override
     public void run() {
@@ -9,53 +15,73 @@ class lab1_3 implements Runnable {
             while (true) {
                 secondsPassed++;
                 System.out.println("Прошло секунд: " + secondsPassed);
-                Thread.sleep(1000);
+                barrier.await(); // Ожидаем, пока другие потоки не будут готовы
+                Thread.sleep(1000); // Ждем 1 секунду
             }
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | BrokenBarrierException e) {
             e.printStackTrace();
         }
     }
 }
 
 class MessageEvery5SecondsTask implements Runnable {
+    private final CyclicBarrier barrier;
+
+    public MessageEvery5SecondsTask(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
     @Override
     public void run() {
         try {
             while (true) {
-                Thread.sleep(5000);
+                for (int i = 0; i < 5; i++) {
+                    barrier.await(); // Ждем 5 секунд
+                }
                 System.out.println("Сообщение каждые 5 секунд.");
             }
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | BrokenBarrierException e) {
             e.printStackTrace();
         }
     }
 }
 
 class MessageEvery7SecondsTask implements Runnable {
+    private final CyclicBarrier barrier;
+
+    public MessageEvery7SecondsTask(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
     @Override
     public void run() {
         try {
             while (true) {
-                Thread.sleep(7000);
+                for (int i = 0; i < 7; i++) {
+                    barrier.await(); // Ждем 7 секунд
+                }
                 System.out.println("Сообщение каждые 7 секунд.");
             }
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | BrokenBarrierException e) {
             e.printStackTrace();
         }
     }
 }
 
-   class MultiThreadTimer {
+class MultiThreadTimer {
     public static void main(String[] args) {
-        TimerTask timerTask = new TimerTask();
-        MessageEvery5SecondsTask messageEvery5SecondsTask = new MessageEvery5SecondsTask();
-        MessageEvery7SecondsTask messageEvery7SecondsTask = new MessageEvery7SecondsTask();
+        CyclicBarrier barrier = new CyclicBarrier(3); // 3 потока: хронометр и два сообщения
+
+        TimerTask timerTask = new TimerTask(barrier);
+        MessageEvery5SecondsTask messageEvery5SecondsTask = new MessageEvery5SecondsTask(barrier);
+        MessageEvery7SecondsTask messageEvery7SecondsTask = new MessageEvery7SecondsTask(barrier);
 
         Thread timerThread = new Thread(timerTask);
         Thread messageEvery5SecondsThread = new Thread(messageEvery5SecondsTask);
         Thread messageEvery7SecondsThread = new Thread(messageEvery7SecondsTask);
+
         timerThread.start();
         messageEvery5SecondsThread.start();
         messageEvery7SecondsThread.start();
     }
- 
+}
