3b78e79576682a8a3a77c8414d59d178bd7f1940
SavitskayaM <savitskayari@yandex.ru>
2024-11-22 11:06:38 +0300

    Update lab1_3.java

--- src/lab1/lab1_3.java
+++ src/lab1/lab1_3.java
@@ -4,65 +4,58 @@ class lab1_3 implements Runnable {
 
     @Override
     public void run() {
-        long startTime = System.currentTimeMillis();
+        int secondsPassed = 0;
         try {
             while (true) {
-                long elapsedTime = System.currentTimeMillis() - startTime;
-                System.out.println("Прошедшее время: " + elapsedTime / 1000 + " секунды");
-                Thread.sleep(1000); // Пауза 1 секунда
+                secondsPassed++;
+                System.out.println("Прошло секунд: " + secondsPassed);
+                Thread.sleep(1000);
             }
         } catch (InterruptedException e) {
-            System.out.println("Поток TimeDisplay был прерван");
-            Thread.currentThread().interrupt(); 
+            e.printStackTrace();
         }
     }
+}
 
+class MessageEvery5SecondsTask implements Runnable {
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Thread.sleep(5000);
+                System.out.println("Сообщение каждые 5 секунд.");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class MessageEvery7SecondsTask implements Runnable {
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Thread.sleep(7000);
+                System.out.println("Сообщение каждые 7 секунд.");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+   class MultiThreadTimer {
     public static void main(String[] args) {
-        // Поток сотображением времени
-        lab1_3 timeDisplay = new lab1_3();
-        Thread timeThread = new Thread(timeDisplay);
-        timeThread.start();
+        TimerTask timerTask = new TimerTask();
+        MessageEvery5SecondsTask messageEvery5SecondsTask = new MessageEvery5SecondsTask();
+        MessageEvery7SecondsTask messageEvery7SecondsTask = new MessageEvery7SecondsTask();
 
-        //5-ти секундная ветка сообщений
-        FiveSecondMessage fiveSecondMessage = new FiveSecondMessage();
-        Thread fiveSecondThread = new Thread(fiveSecondMessage);
-        fiveSecondThread.start();
-
-        // 7-ми секундная ветка сообщений
-        SevenSecondMessage sevenSecondMessage = new SevenSecondMessage();
-        Thread sevenSecondThread = new Thread(sevenSecondMessage);
-        sevenSecondThread.start();
-    }            
-}
-
-class FiveSecondMessage implements Runnable {
-
-    @Override
-    public void run() {
-        try {
-            while (true) {
-                System.out.println("Сообщение каждые 5 секунд");
-                Thread.sleep(5000); // Пауза в 5 секунд
-            }
-        } catch (InterruptedException e) {
-            System.out.println("\n" + "Поток FiveSecondMessage был прерван.");
-            Thread.currentThread().interrupt();
-        }
+        Thread timerThread = new Thread(timerTask);
+        Thread messageEvery5SecondsThread = new Thread(messageEvery5SecondsTask);
+        Thread messageEvery7SecondsThread = new Thread(messageEvery7SecondsTask);
+        timerThread.start();
+        messageEvery5SecondsThread.start();
+        messageEvery7SecondsThread.start();
     }
-}
-
-class SevenSecondMessage implements Runnable {
-
-    @Override
-    public void run() {
-        try {
-            while (true) {
-                System.out.println("Сообщение каждые 7 секунд");
-                Thread.sleep(7000); // Пуза в 7 секунд
-            }
-        } catch (InterruptedException e) {
-            System.out.println("Поток SevenSecondMessage прерван.");
-            Thread.currentThread().interrupt(); 
-        }
-    }
-}
+ 
