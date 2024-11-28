a3f9be52212427e3f8feacf51ec58cd3525d542c
SavitskayaM <savitskayari@yandex.ru>
2024-11-28 11:27:42 +0300

    Update lab2.java

--- src/lab1/lab2.java
+++ src/lab1/lab2.java
@@ -1,18 +1,17 @@
-package lab1;
-
 import java.util.LinkedList;
 import java.util.Queue;
-  
+
 class Buffer {
     private final int capacity;
     private final Queue<Integer> queue = new LinkedList<>();
 
-    public Buffer (int capacity){
+    public Buffer(int capacity) {
         this.capacity = capacity;
     }
 
-    public synchronized void produce (int value) throws InterruptedException {
-        while(queue.size() == capacity){
+    public synchronized void produce(int value) throws InterruptedException {
+        while (queue.size() == capacity) {
+            System.out.println("Буфер заполнен, производитель ждет...");
             wait();
         }
         queue.add(value);
@@ -21,7 +20,8 @@ class Buffer {
     }
 
     public synchronized int consume() throws InterruptedException {
-        while (queue.isEmpty()){
+        while (queue.isEmpty()) {
+            System.out.println("Буфер пуст, потребитель ждет...");
             wait();
         }
         int value = queue.poll();
@@ -31,16 +31,18 @@ class Buffer {
     }
 }
 
-class Producer implements Runnable{
+class Producer implements Runnable {
     private final Buffer buffer;
-    public Producer(Buffer buffer){
+
+    public Producer(Buffer buffer) {
         this.buffer = buffer;
     }
+
     @Override
-    public void run(){
+    public void run() {
         int value = 0;
         try {
-            while(true){
+            while (true) {
                 buffer.produce(value++);
                 Thread.sleep(500);
             }
@@ -52,24 +54,25 @@ class Producer implements Runnable{
 
 class Consumer implements Runnable {
     private final Buffer buffer;
-    public Consumer(Buffer buffer){
+
+    public Consumer(Buffer buffer) {
         this.buffer = buffer;
     }
 
     @Override
     public void run() {
         try {
-            while (true){
+            while (true) {
                 buffer.consume();
                 Thread.sleep(750);
             }
-        } catch (InterruptedException e){
+        } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
     }
 }
 
-public class ProducerConsumer {
+class ProducerConsumer {
     public static void main(String[] args) {
         Buffer buffer = new Buffer(5);
         Thread producerThread = new Thread(new Producer(buffer));
